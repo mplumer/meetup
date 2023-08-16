8e8239b0c9becd6fde55a929bc12b9493bacff7e
Max Plumer <maxplumer12@gmail.com>
2023-08-16 16:40:59 -0400

    detail functionality, styling

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail 
+    image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Fronalpstock_big.jpg/800px-Fronalpstock_big.jpg"
+    title="The First Meetup"
+    address="Some Street 5, Some City"
+    description="This is a first meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
