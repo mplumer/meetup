06a2deca94cf33037c01bc1175166dd11fe1d8ac
Max Plumer <maxplumer12@gmail.com>
2023-08-17 16:30:08 -0400

    mongo

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,17 +1,16 @@
 import { MongoClient } from 'mongodb';
-//import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-//dotenv.config();
+dotenv.config();
 
-//const MONGODB = process.env.MONGODB_URI;
+const MONGODB = process.env.MONGODB_URI;
 
 async function handler(req, res) {
    if (req.method === 'POST') {
         const data = req.body;
 
         const client = await MongoClient.connect(
-         'mongodb+srv://maxwell:mongodongo@cluster0.ug4t9.mongodb.net/meetups?retryWrites=true&w=majority'
-         //`${MONGODB}`
+         `${MONGODB}`
          ); 
         const db = client.db();
 
